c971b0924ffb56ee00bd1712d06b19649944e1a9
iqraabashir <iqrabashir729@gmail.com>
2024-11-01 13:35:57 +0530

    arithematic operators

---
+++ operators.py
@@ -0,0 +1,9 @@
+a = 32     
+b = 6  
+print('Addition of two numbers:',a+b)  
+print('Subtraction of two numbers:',a-b)  
+print('Multiplication of two numbers:',a*b)  
+print('Division of two numbers:',a/b)  
+print('Reminder of two numbers:',a%b)  
+print('Exponent of two numbers:',a**b)  
+print('Floor division of two numbers:',a//b)  
